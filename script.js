d64b789371098df3d07f0a37847b1ed34115241b
Jonathan J. Watson <jonathanwatson1@gmail.com>
2022-07-05 20:40:33 -0400

    phase 3 complete

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 console.log("Hello!");
+
+let imageURL =
+  "https://media2.giphy.com/media/3orif7XXtgQZirOw1y/giphy.gif?cid=adfa8aa387ecd7e7c7bf61990874c5f28cc5ea6a3b6eb2c3&rid=giphy.gif&ct=g";
+
+const imageEl = document.getElementById("my-gif");
+
+const displayImage = () => {
+  imageEl.src = imageURL;
+};
+
+
 const requestOptions = {
   method: "GET",
   redirect: "follow",
@@ -12,5 +23,7 @@ fetch(
   .then((result) => {
     console.log(result);
     console.log(result.data.images.original.url);
+    imageURL = result.data.images.original.url;
+    displayImage();
   })
   .catch((error) => console.log("error", error));
